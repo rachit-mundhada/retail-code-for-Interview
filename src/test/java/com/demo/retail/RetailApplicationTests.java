997c27f8c5dbf1fb6bdb9e81d77ffe1eb974fe3f
Rachit Mundhada <mundhadarachit22@gmail.com>
2023-06-01 08:57:45 +0530

    Committing changes for providing loggers

--- src/test/java/com/demo/retail/RetailApplicationTests.java
+++ src/test/java/com/demo/retail/RetailApplicationTests.java
@@ -1,14 +1,26 @@
 package com.demo.retail;
 
+import com.demo.retail.service.RetailsServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@WebMvcTest
 class RetailApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private RetailsServiceImpl retailsService;
 
 	@Test
 	void checkIfAmtIsNull(){}
